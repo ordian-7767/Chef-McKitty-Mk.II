bf3a3fc04933490f15a038772f75c97f412697ff
ordian-7767 <mordiansyah13th@gmail.com>
2022-10-17 11:39:17 +0700

    Change SW Configure

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -6,7 +6,7 @@ const swRegister = async () => {
     return;
   }
 
-  const wb = new WorkboxWindow.Workbox('../sw.js');
+  const wb = new WorkboxWindow.Workbox('./sw.bundle.js');
 
   try {
     await wb.register();
